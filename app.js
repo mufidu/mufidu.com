a4e479455bec42cd057fa98d17c127b75a440cd0
Muhammad Mufid Utomo <70360519+mufidu@users.noreply.github.com>
2021-02-21 20:58:40 +0700

    update color 2.0

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ button.addEventListener("click", function () {
 
   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
-  h1.textContent = `Now it is ${Math.floor(100 * r / 255)}% red, ${Math.floor(100 * g / 255}% green, and ${Math.floor(100 * b / 255}% blue.`;
+  h1.textContent = `Now it is ${Math.floor((100 * r) / 255)}% red, ${Math.floor(
+    (100 * g) / 255
+  )}% green, and ${Math.floor((100 * b) / 255)}% blue.`;
 
-  h1.style.color = `rgb(${Math.abs(r - 255)}, ${Math.abs(
-    g - 255
-  )}, ${Math.abs(b - 255)})`;
+  h1.style.color = `rgb(${Math.abs(r - 255)}, ${Math.abs(g - 255)}, ${Math.abs(
+    b - 255
+  )})`;
 });
