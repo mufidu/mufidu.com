06bb9d22b9cae74f6ccaa0b5ba3a556742a357b8
Muhammad Mufid Utomo <70360519+mufidu@users.noreply.github.com>
2021-02-22 10:26:49 +0700

    update 3.0

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const h1 = document.querySelector("h1");
+const texts = document.querySelector(".colorNow");
+const project = document.querySelector(".projectName");
 const button = document.querySelector("button");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function (e) {
+  e.preventDefault();
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
 
   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
-  h1.textContent = `Now it is ${Math.floor((100 * r) / 255)}% red, ${Math.floor(
-    (100 * g) / 255
-  )}% green, and ${Math.floor((100 * b) / 255)}% blue.`;
+  texts.textContent = `Now it is ${Math.floor(
+    (100 * r) / 255
+  )}% red, ${Math.floor((100 * g) / 255)}% green, and ${Math.floor(
+    (100 * b) / 255
+  )}% blue.`;
 
-  h1.style.color = `rgb(${Math.abs(r - 255)}, ${Math.abs(g - 255)}, ${Math.abs(
-    b - 255
-  )})`;
+  texts.style.color = `rgb(${Math.abs(r - 255)}, ${Math.abs(
+    g - 255
+  )}, ${Math.abs(b - 255)})`;
+
+  project.style.color = `rgb(${Math.abs(r - 255)}, ${Math.abs(
+    g - 255
+  )}, ${Math.abs(b - 255)})`;
 });
